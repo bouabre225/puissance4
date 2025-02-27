744f6d87b4b66599fc882f34ddb76004874f731f
Ange KORE <angebouabre463@gmail.com>
2025-02-27 14:29:54 +0100

    mise en place des structures matrice et joueurs

--- main.c
+++ main.c
@@ -4,7 +4,18 @@
 #include <conio.h>
 #include <windows.h>
 
+typedef struct Matrice;
+struct Matrice
+{
+    char Mat [6][7];
+};
 
+typedef struct Joueurs;
+struct Joueurs
+{
+    char nom [100];
+    char symbole;
+};
 
 int main ()
 {
